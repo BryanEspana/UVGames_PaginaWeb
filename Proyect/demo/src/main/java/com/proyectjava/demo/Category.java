259ba41c3868d50d0dbd7b4a008741de8f6115ac
Javilejoo <83861800+Javilejoo@users.noreply.github.com>
2022-05-31 12:57:47 -0600

    Add files via upload
    Implementacion de categoria

---
+++ Proyect/demo/src/main/java/com/proyectjava/demo/Category.java
@@ -0,0 +1,12 @@
+package com.proyectjava.demo;
+
+public class Category {
+    private final String name;
+    public Category(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    
+}
