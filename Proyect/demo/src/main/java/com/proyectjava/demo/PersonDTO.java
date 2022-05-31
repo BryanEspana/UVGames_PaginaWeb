0128afc347d9ba1130af4770f31ae7e8acbf8968
Bryan España <83832357+BryanEspana@users.noreply.github.com>
2022-05-31 12:45:47 -0600

    Add files via upload

---
+++ Proyect/demo/src/main/java/com/proyectjava/demo/PersonDTO.java
@@ -0,0 +1,15 @@
+package com.proyectjava.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+    private String username;
+    private String password;
+}
